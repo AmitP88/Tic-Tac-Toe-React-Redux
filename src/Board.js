c43adcad77eab0986b1890991a99c02a1e0968a2
amitp <amitpatel_7@msn.com>
2018-11-14 06:46:15 -0800

    changed styles of Board and increased font of .cell elements

--- src/Board.js
+++ src/Board.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-const X = ' X ';
-const O = ' O ';
+        const X = ' X ';
+        const O = ' O ';
 
 class Board extends Component {
     constructor(props) {
@@ -13,25 +13,35 @@ class Board extends Component {
                 ['','','']
             ]
         }
+
+        this.handleOnClick = this.handleOnClick.bind(this);
+    }
+
+    handleOnClick(event) {
+        if(event.target.innerHTML === ''){
+            event.target.innerHTML = X;
+        }
+
+        console.log(event.target.innerHTML);
     }
 
     render() {
         return (
             <div className="board-container">
                 <div className="row">
-                    <div className="cell">{this.state.board[0][0]}</div>
-                    <div className="cell">{this.state.board[0][1]}</div>
-                    <div className="cell">{this.state.board[0][2]}</div>
+                    <div className="cell" id="cell-1" onClick={this.handleOnClick}>{this.state.board[0][0]}</div>
+                    <div className="cell" id="cell-2" onClick={this.handleOnClick}>{this.state.board[0][1]}</div>
+                    <div className="cell" id="cell-3" onClick={this.handleOnClick}>{this.state.board[0][2]}</div>
                 </div>
                 <div className="row">
-                    <div className="cell">{this.state.board[1][0]}</div>
-                    <div className="cell">{this.state.board[1][1]}</div>
-                    <div className="cell">{this.state.board[1][2]}</div>
+                    <div className="cell" id="cell-4" onClick={this.handleOnClick}>{this.state.board[1][0]}</div>
+                    <div className="cell" id="cell-5" onClick={this.handleOnClick}>{this.state.board[1][1]}</div>
+                    <div className="cell" id="cell-6" onClick={this.handleOnClick}>{this.state.board[1][2]}</div>
                 </div>
                 <div className="row">
-                    <div className="cell">{this.state.board[2][0]}</div>
-                    <div className="cell">{this.state.board[2][1]}</div>
-                    <div className="cell">{this.state.board[2][2]}</div>
+                    <div className="cell" id="cell-7" onClick={this.handleOnClick}>{this.state.board[2][0]}</div>
+                    <div className="cell" id="cell-8" onClick={this.handleOnClick}>{this.state.board[2][1]}</div>
+                    <div className="cell" id="cell-9" onClick={this.handleOnClick}>{this.state.board[2][2]}</div>
                 </div>
             </div>
         );
