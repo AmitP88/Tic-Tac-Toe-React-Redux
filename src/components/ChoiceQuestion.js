b198305206728c7ff8798b670ce62b0691c60c9f
amitp <amitpatel_7@msn.com>
2018-11-19 09:51:31 -0800

    turned ChoiceQuestion component into a class component and added local state along with a new method called handleChoice

--- src/components/ChoiceQuestion.js
+++ src/components/ChoiceQuestion.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React ,{Component} from 'react';
 
-const ChoiceQuestion = (X = ' X ', O = ' O ') => {
-    const handleChoice = (event) => {
-        console.log('handleChoice');
+class ChoiceQuestion extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            player: '',
+            computer: ''
+        }
+
+        this.handleChoice = this.handleChoice.bind(this);
     }
 
-    return (
-        <div className="choice-container">
-            <h3 className="choice-question">
-                Play as 
-                <span onClick={handleChoice()}>X</span>
-                or
-                <span onClick={handleChoice()}>O</span>
-                ?
-            </h3>                
-        </div>
-    );
+
+    handleChoice(e) {
+        if(e.target.id === "X"){
+            this.setState({
+                player: 'X',
+                computer: 'O'
+            }, () => console.log('player: ', this.state.player, 'computer: ', this.state.computer));
+        } else if(e.target.id === "O"){
+            this.setState({
+                player: 'O',
+                computer: 'X'
+            }, () => console.log('player: ', this.state.player, 'computer: ', this.state.computer));            
+        }
+    }
+
+    render() {
+        return (
+            <div className="choice-container">
+                <h3 className="choice-question">
+                    Play as 
+                    <span id="X" onClick={this.handleChoice}>X</span>
+                    or
+                    <span id="O" onClick={this.handleChoice}>O</span>
+                    ?
+                </h3>                
+            </div>
+        );        
+    }
+
 }
 
 export default ChoiceQuestion;
