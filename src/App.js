55e1b9500bda2dc6e527d9a2c00608cf09643fa3
amitp <amitpatel_7@msn.com>
2018-11-19 18:18:20 -0800

    implemented redux inside ChoiceQuestion component

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ import ChoiceAnswer from './components/ChoiceAnswer';
 import Winner from './components/Winner';
 import Board from './components/Board';
 
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Tic Tac Toe</h1>
-        <ChoiceQuestion />
-        <ChoiceAnswer />
-        <Board />
-        <Winner />
-      </div>
+        <div className="App">
+          <h1>Tic Tac Toe</h1>
+          <ChoiceQuestion />
+          <ChoiceAnswer />
+          <Board />
+          <Winner />
+        </div>
     );
   }
 }
--- src/components/ChoiceQuestion.js
+++ src/components/ChoiceQuestion.js
@@ -1,28 +1,61 @@
-import React ,{Component} from 'react';
+import React ,{ Component } from 'react';
+import { createStore } from 'redux';
 
-class ChoiceQuestion extends Component {
+const X = ' X ';
+const O = ' O ';
+
+const initialState = {
+    player: '',
+    computer: ''
+}
+
+// Step 2: Reducer - state n action
+const reducer = (state = initialState, action) => {
+    if(action.type === "PLAYER_X"){
+        return action.payload;
+    }
+
+    if(action.type === "PLAYER_O"){
+        return action.payload;
+    }            
+
+    return state;
+}
+
+// Step 1: Store - reducer n state
+export const store = createStore(reducer, initialState);
+
+// Step 3: Subscribe
+store.subscribe(() => {
+    console.log("Store is now", store.getState());
+});
+
+
+export default class ChoiceQuestion extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            player: '',
-            computer: ''
-        }
-
         this.handleChoice = this.handleChoice.bind(this);
     }
 
-
     handleChoice(e) {
         if(e.target.id === "X"){
-            this.setState({
-                player: 'X',
-                computer: 'O'
-            }, () => console.log('player: ', this.state.player, 'computer: ', this.state.computer));
+            // Step 4: Dispatch action
+            store.dispatch({
+                type: "PLAYER_X",
+                payload: {
+                    player: X,
+                    computer: O
+                }
+            });
         } else if(e.target.id === "O"){
-            this.setState({
-                player: 'O',
-                computer: 'X'
-            }, () => console.log('player: ', this.state.player, 'computer: ', this.state.computer));            
+            // Step 4: Dispatch action
+            store.dispatch({
+                type: "PLAYER_O",
+                payload: {
+                    player: O,
+                    computer: X
+                }
+            });
         }
     }
 
@@ -39,7 +72,4 @@ class ChoiceQuestion extends Component {
             </div>
         );        
     }
-
 }
-
-export default ChoiceQuestion;
