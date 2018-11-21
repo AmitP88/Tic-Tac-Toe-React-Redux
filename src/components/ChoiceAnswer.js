640e833b7acae495c6abe7235a9d47eef13f5288
amitp <amitpatel_7@msn.com>
2018-11-20 19:56:19 -0800

    prevented text highlighting on ChoiceQuestion component

--- src/components/ChoiceAnswer.js
+++ src/components/ChoiceAnswer.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import store from '../store/store';
-
 export class ChoiceAnswer extends Component {
     render() {
         console.log(this.props.player);        
         return (
             <h3 className="choice-statement">You are {this.props.player}</h3>
-        );        
+        );
     }
 }
 
