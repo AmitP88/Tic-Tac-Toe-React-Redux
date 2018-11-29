ed1f189bc1aa2615f3e5dc1a42feda4fb500981b
amitp <amitpatel_7@msn.com>
2018-11-29 00:57:13 -0800

    changed switch statement to if else statement

--- src/reducers/winnerReducer.js
+++ src/reducers/winnerReducer.js
@@ -2,21 +2,20 @@ import { initialState } from '../store/store';
 import { COMPUTER_WINS, PLAYER_WINS, DRAW } from '../actions/winnerActions';
 
 const winnerReducer = (state = initialState, action) => {
-    switch(action.type){
-        case COMPUTER_WINS:
-            return {
-                winner: 'Computer'
-            };
-        case PLAYER_WINS:
-            return {
-                winner: 'You'
-            };
-        case DRAW:
-            return {
-                winner: 'Draw'
-            };
-        default:
-            return state;
+    if(action.type === COMPUTER_WINS){
+        return {
+            winner: 'Computer'
+        }
+    } else if(action.type === PLAYER_WINS){
+        return {
+            winner: 'You'
+        }
+    } else if(action.type === DRAW){
+        return {
+            winner: 'Draw'
+        }
+    } else {
+        return state;
     }
 }
 
