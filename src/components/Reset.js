627308db0cb25cf2a92f3afeac939a17400d52a7
amitp <amitpatel_7@msn.com>
2018-11-30 05:58:35 -0800

    unmounted Reset component on default. Mounted Reset component when choice state updates

--- src/components/Reset.js
+++ src/components/Reset.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { store } from '../store/store';
+import { connect } from 'react-redux';
 import resetGame from '../actions/resetActions';
 
 class Reset extends Component {
@@ -14,11 +15,21 @@ class Reset extends Component {
         console.log('Resets the game!');
     }
     render() {
-        return (
-            <button className="resetButton" onClick={this.handleReset}>Reset</button>
-        );        
+        if(this.props.player){
+            return (
+                <button className="resetButton" onClick={this.handleReset}>Reset</button>
+            );              
+        } else {
+            return null;
+        }
     }
-
 }
 
-export default Reset;
+const mapStateToProps = (state) => {
+    console.log(state);
+    return {
+        player: state.choiceReducer.player
+    };
+}
+
+export default connect(mapStateToProps)(Reset);
