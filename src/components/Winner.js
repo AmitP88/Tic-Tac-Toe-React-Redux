051bb87765d5bdcb35ef7012630027b705b30370
amitp <amitpatel_7@msn.com>
2018-11-30 07:32:15 -0800

    installed react-modal npm package and wrapped Winner component inside ReactModal component from react-modal

--- src/components/Winner.js
+++ src/components/Winner.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import ReactModal from 'react-modal';
 import Reset from '../components/Reset';
 
 class Winner extends Component {
+    componentWillMount() {
+        ReactModal.setAppElement('body');
+    }
+
     render() {
         let endgame = this.props.endgame;
         let winner;
@@ -12,10 +17,10 @@ class Winner extends Component {
         } else if(endgame !== ''){
             winner = <h3 className="winner">Winner: {this.props.endgame}</h3>
             return (
-                <div className="winner-container">
+                <ReactModal className="winner-container" isOpen={true}>
                     {winner}
-                    <Reset />
-                </div>
+                    <Reset />              
+                </ReactModal>
             );  
         }
     }
