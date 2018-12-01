696b24c64da50fd995a628c51cf27d10f063f337
amitp <amitpatel_7@msn.com>
2018-12-01 10:35:45 -0800

    added p element with rotating react and redux svg icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import react from '../react.svg';
+import redux from '../redux.svg';
 
 class Footer extends Component {
     render() {
       return (
         <footer>
+          <p className="builtWith">
+            Built with <img src={react} className="App-logo" alt="logo" /> + <img src={redux} className="App-logo" alt="logo" />
+          </p>
+            
             <a href="https://github.com/AmitP88/Tic-Tac-Toe-React-Redux" target="_blank" rel="noopener noreferrer">
             <i className="fa fa-github fa-4x"></i>
             </a>      
