83723d2c38f6dbd159f1307391e0969e274329e9
amitp <amitpatel_7@msn.com>
2018-12-01 11:45:28 -0800

    Updates

---
+++ precache-manifest.6328919f8712691ebbd471bea4bc181b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "319affeb3e9915a4de6785cdb0c8d355",
+    "url": "/Tic-Tac-Toe-React-Redux/static/media/redux.319affeb.svg"
+  },
+  {
+    "revision": "fb7449d273b49a602b46",
+    "url": "/Tic-Tac-Toe-React-Redux/static/css/main.c1fd686e.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Tic-Tac-Toe-React-Redux/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "ff2ff89bcb94d8774f58",
+    "url": "/Tic-Tac-Toe-React-Redux/static/js/1.ff2ff89b.chunk.js"
+  },
+  {
+    "revision": "99e26450d3752f6c12d9",
+    "url": "/Tic-Tac-Toe-React-Redux/static/js/runtime~main.99e26450.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tic-Tac-Toe-React-Redux/static/media/react.5d5d9eef.svg"
+  },
+  {
+    "revision": "fb7449d273b49a602b46",
+    "url": "/Tic-Tac-Toe-React-Redux/static/js/main.fb7449d2.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Tic-Tac-Toe-React-Redux/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Tic-Tac-Toe-React-Redux/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Tic-Tac-Toe-React-Redux/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Tic-Tac-Toe-React-Redux/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "ff2ff89bcb94d8774f58",
+    "url": "/Tic-Tac-Toe-React-Redux/static/css/1.bdd2b4bd.chunk.css"
+  },
+  {
+    "revision": "1861e53786c04cba84864468eb50fe63",
+    "url": "/Tic-Tac-Toe-React-Redux/index.html"
+  }
+];
